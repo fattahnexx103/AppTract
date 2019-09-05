f569c3580e181c53f5b37f0c2223e0dd39415695
fattahnexx103 <nexxfattah@gmail.com>
2019-09-04 23:35:54 -0400

    Added firebase and implemented sign up functionality

--- app/src/main/java/apps/android/fattahnexx103/apptract/activities/SignUpActivity.kt
+++ app/src/main/java/apps/android/fattahnexx103/apptract/activities/SignUpActivity.kt
@@ -5,9 +5,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import apps.android.fattahnexx103.apptract.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+
+        //check user
+        val user = firebaseAuth.currentUser
+        if(user != null){
+            //this is in listener because this always checks in the status so user can auto log in
+            startActivity(MainActivity.newIntent(this)) //redirects back to Mainactivity
+            finish() //finish current activity
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +31,29 @@ class SignUpActivity : AppCompatActivity() {
 
     }
 
-    fun onSignup(view: View){
-        startActivity(MainActivity.newIntent(this)) //redirects back to Mainactivity
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener) //set the auth listener at startup
     }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener) //remove the listener
+    }
+
+    fun onSignup(view: View){
+        //if the email and and pass are null or empty
+        if(!editText_email.text.toString().isNullOrEmpty() && !editText_password.text.toString().isNullOrEmpty()){
+            firebaseAuth.createUserWithEmailAndPassword(editText_email.text.toString(),editText_password.text.toString()) //create the firebase user
+                .addOnCompleteListener {task ->
+                    if(!task.isSuccessful){
+                        //if task is not successful then give an error
+                        Toast.makeText(this, "Sign up Failed .. ${task.exception?.localizedMessage}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
 
     //static function
     companion object {
